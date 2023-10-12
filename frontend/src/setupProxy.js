3e5464882e4edb3583e833d0b76b089e69190db2
HafizDa <hafizs@metropolia.fi>
2023-10-12 03:00:17 +0300

    deploying

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://127.0.0.1:3001",
+      target: "http://127.0.0.1:5001",
       changeOrigin: true,
       secure: false,
     })
